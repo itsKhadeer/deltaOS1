5054ae990f8776ffd6073792656d6cadd6230871
itsKhadeer <119066611+itsKhadeer@users.noreply.github.com>
2023-08-10 23:59:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from enum import Enum
-from typing import List, Annotated
+from datetime import datetime;
 import mysql.connector
 
 app = FastAPI()
@@ -13,14 +12,16 @@ db_config = {
     "password": "Khan@2004",
     "database": "deltaos1"
 }
+
 db_conn = mysql.connector.connect(**db_config)
 db_cursor = db_conn.cursor(dictionary=True)
+
 
 
 # Endpoint to get flights at a specific airport on a given day
 @app.get("/airport/{airport_id}")
 def get_flights_at_airport(airport_id: int, ):
-    query = "SELECT name FROM Flights WHERE departure_airport_id = %s OR arrival_airport_id = %s"
+    query = "SELECT * FROM Flights WHERE departure_airport_id = %s OR arrival_airport_id = %s"
     db_cursor.execute(query, (airport_id, airport_id))
     flights = db_cursor.fetchall()
     if(flights):
@@ -34,19 +35,14 @@ class location(str, Enum):
     Location1 = "Location 1"
     Location2 = "Location 2"
     Location3 = "Location 3"
-    Location4 = "Location 4"
-    Location5 = "Location 5"
-    Location6 = "Location 6"
-    Location7 = "Location 7"
-    Location8 = "Location 8"
-    Location9 = "Location 9"
-    Location10 = "Location 10"
-    Location11 = "Location 11"
+
+
+
 
 @app.get("/nextFlight/{time}/{destination}")
-def get_next_flight(time: int, destination: location):
+def get_next_flight(time: datetime, destination: location):
 
-    if(time > 24 or time < 0) :
+    if(time.hour > 24 or time.hour < 0) :
         raise HTTPException(status_code=400, detail="invalid time input")
     
     querry = "select * from flights where departure_time > %s and arrival_airport_id = (select id from airports where location = %s)"
@@ -54,19 +50,53 @@ def get_next_flight(time: int, destination: location):
     next_flight = db_cursor.fetchall()
     if next_flight:
         return next_flight
-    else :
-        return f"no flights going to {destination} available after {time} hours today"
     
+    else :
+        return f"no flights going to {destination} available after {time.time} hours today"
+    
+
 
 
 # Endpoint to find the busiest airport at a given hour
 @app.get("/busiest/{time}")
 def get_busiest_airport(time: int):
-    #need to figure this out
-    busiest_airport = "need to figure this out"
-    return busiest_airport
 
+    querry = "select departure_airport_id from flights where hour(departure_time) = %s"
+    db_cursor.execute(querry, (time,))
+    the_table = db_cursor.fetchall()
+    print(the_table)
+    frequency_dict = {}
+    for rows in the_table: 
 
+        if rows["departure_airport_id"] in frequency_dict.keys() :
+            frequency_dict[rows["departure_airport_id"]] += 1
+        
+        else : 
+            frequency_dict[rows["departure_airport_id"]] = 1
+    print(frequency_dict)
+
+    querry = "select arrival_airport_id from flights where hour(arrival_time) = %s"
+    db_cursor.execute(querry, (time,))
+    the_table = db_cursor.fetchall()
+    
+    for rows in the_table: 
+
+        if rows["arrival_airport_id"] in frequency_dict.keys() :
+            frequency_dict[rows["arrival_airport_id"]] += 1
+        
+        else : 
+            frequency_dict[rows["arrival_airport_id"]] = 1
+    
+    if frequency_dict:
+        max_val = sorted(frequency_dict.values(), reverse=True)
+        Keymax = max_val[0]
+        print(frequency_dict)
+        querry="select name,location from airports where id = %s"
+        db_cursor.execute(querry,(Keymax,))
+        return db_cursor.fetchall()
+    return "there are no flights landing or taking off at the given hour in any airport"
+            
+        
 # Close the database connection after request
 @app.on_event("shutdown")
 def shutdown_db_conn():
